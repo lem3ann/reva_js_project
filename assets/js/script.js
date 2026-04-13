f6077b2b077b18298fb5fd2f279daafd380c35c9
ll3m4n <laman@cubics.technology>
2026-04-13 17:35:05 +0400

    create:creating portfolio.html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,19 @@
 let backgroundImg = document.getElementsByClassName("right-mainpage")[0];
 let moveableTextNode = document.getElementsByClassName("moveable-text")[0];
 // sekil deyisme hissesi
-let bgPicArray = ["../assets/images/1.jpg", "../assets/images/2.jpg", "../assets/images/3.jpg"]
+let bgPicArray = [
+  "../assets/images/1.jpg",
+  "../assets/images/2.jpg",
+  "../assets/images/3.jpg",
+];
 let imgIndex = 0;
 
 function changeBackground() {
-    backgroundImg.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${bgPicArray[imgIndex]})`;
-    imgIndex++;
-    if (imgIndex === bgPicArray.length) {
-        imgIndex = 0;
-    }
-
+  backgroundImg.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${bgPicArray[imgIndex]})`;
+  imgIndex++;
+  if (imgIndex === bgPicArray.length) {
+    imgIndex = 0;
+  }
 }
 setInterval(changeBackground, 5000);
 
@@ -22,37 +25,37 @@ let flag = true;
 
 // tek-tek string silen function
 function removeTextString() {
-    if (textVar.length > 0) {
-        textVar = textVar.slice(0, -1);
-        showText(textVar);
-    } else {
-        flag = false;
-    }
+  if (textVar.length > 0) {
+    textVar = textVar.slice(0, -1);
+    showText(textVar);
+  } else {
+    flag = false;
+  }
 }
 // elementi gostermek ucun function
 function showText(text) {
-    moveableTextNode.textContent = text;
+  moveableTextNode.textContent = text;
 }
 // function write forward
 
 let k = 0;
 
 function writeForward() {
-    if (k < secondLine.length) {
-        textVar = textVar + secondLine[k];
-        showText(textVar);
-        k++;
-    }
-    if (k === secondLine.length) {
-        setTimeout(removeTextString, 1000);
-    }
+  if (k < secondLine.length) {
+    textVar = textVar + secondLine[k];
+    showText(textVar);
+    k++;
+  }
+  if (k === secondLine.length) {
+    setTimeout(removeTextString, 1000);
+  }
 }
 setInterval(() => {
-    if (flag) {
-        removeTextString();
-    } else {
-        writeForward();
-    }
+  if (flag) {
+    removeTextString();
+  } else {
+    writeForward();
+  }
 }, 100);
 // LEFT AND RIGHT CARET ICON FUNCTIONALITY
 let leftArrow = document.querySelectorAll(".positions-arrow button")[0];
@@ -60,21 +63,27 @@ let rightArrow = document.querySelectorAll(".positions-arrow button")[1];
 leftArrow.addEventListener("click", changeBackground);
 rightArrow.addEventListener("click", changeBackground);
 
-// // ##########################  CARET ICONU ILE NAVBAR KICILMESI ##########################
-// let leftCaretIcon = document.getElementsByClassName("border-and-color")[0];
-// leftCaretIcon.addEventListener("click", function () {
-//     backgroundImg.style.width = "3000px";
-//     backgroundImg.style.overflow = "hidden";
-// })
-
-// ##########################   NAVTABS MENTIQI ####################################3
-let allList = document.querySelectorAll("ul li");
+// !       ##########################   NAVTABS MENTIQI ####################################3
+let allTab = document.querySelectorAll("ul li");
+let allListContent = document.querySelectorAll(".about-navtab-content");
 // console.log(allList);
-for (let i = 0; i < allList.length; i++) {
-    allList[i].addEventListener("click", function () {
-        allList.forEach(element => {
-            element.classList.remove("active");
-        });
-        allList[i].classList.add("active");
+
+for (let i = 0; i < allTab.length; i++) {
+  allTab[i].addEventListener("click", function () {
+    allTab.forEach((element) => {
+      element.classList.remove("active");
     });
-};
+    allTab[i].classList.add("active");
+  });
+}
+
+// ul listde click olunanda bordere top vermek
+let ulListTopBorder = document.querySelectorAll(".flex-ul-list li ");
+for (let i = 0; i < ulListTopBorder.length; i++) {
+  ulListTopBorder[i].addEventListener("click", function () {
+    ulListTopBorder.forEach((element) => {
+      element.classList.remove("style-top-border");
+    });
+    ulListTopBorder[i].classList.add("style-top-border");
+  });
+}
