51371a2cdac5d3d10c12803ebf5b52f4ec7cf4d8
learngit <ltfv.lmn@gmail.com>
2026-04-16 00:58:11 +0400

    fix:update main page left side navbar transition and maps functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,23 @@ let headerCaretIcon = document.getElementsByClassName("border-and-color")[0];
 let backgroundImg = document.getElementsByClassName("right-mainpage")[0];
 let ulListIcons = document.querySelectorAll(".navbar ul li p");
 let leftVertical = document.getElementsByClassName("left-vertical")[0];
+let navbarFlag = true;
 headerCaretIcon.addEventListener("click", function () {
-  for (let i = 0; i < ulListIcons.length; i++) {
-    ulListIcons[i].style.display = "none";
-    leftVertical.style.width = "6%";
-    backgroundImg.style.width = "94%";
+  if (navbarFlag) {
+    for (let i = 0; i < ulListIcons.length; i++) {
+      ulListIcons[i].style.display = "none";
+      leftVertical.style.width = "6%";
+      backgroundImg.style.width = "94%";
+    }
+    navbarFlag = false;
+  } else {
+    for (let i = 0; i < ulListIcons.length; i++) {
+      console.log("test");
+      ulListIcons[i].style.display = "flex";
+      leftVertical.style.width = "14%";
+      backgroundImg.style.width = "86%";
+      navbarFlag = true;
+    }
   }
 });
 
@@ -140,37 +152,38 @@ for (let i = 0; i < allImagesButton.length; i++) {
 }
 
 // ? PORTFOLIO CREATE WHITE BLUR
-let portfolioImgContainer = document.querySelectorAll(".portfolio-first div");
-// let test = document.querySelectorAll(".portfolio-first");
-// console.log(portfolioImgContainer);
-let blurContainer = document.createElement("div");
-for (let i = 0; i < portfolioImgContainer.length; i++) {
-  portfolioImgContainer[i].addEventListener("mouseenter", function () {
-    blurContainer.classList.add("dark-blur");
-    // console.log(blurContainer.parentNode);
-    portfolioImgContainer[i].appendChild(blurContainer);
-  });
-}
+// let portfolioImgContainer = document.querySelectorAll(".portfolio-first div");
+// // let test = document.querySelectorAll(".portfolio-first");
+// // console.log(portfolioImgContainer);
+// let blurContainer = document.createElement("div");
+// for (let i = 0; i < portfolioImgContainer.length; i++) {
+//   portfolioImgContainer[i].addEventListener("mouseenter", function () {
+//     blurContainer.classList.add("dark-blur");
+//     // console.log(blurContainer.parentNode);
+//     portfolioImgContainer[i].appendChild(blurContainer);
+//   });
+// }
 
 // CONTACT PAGE ---maps functionality
 let mapButton = document.getElementsByClassName("app-length")[0];
 let mapIframe = document.querySelector(".map-frame iframe");
+let mapHeight = document.querySelector(".map-frame");
 let myFlag = false;
-let mapButtonContent =
-  document.querySelector(".app-length").children[1].textContent;
-mapButton.addEventListener("click", function () {
-  if (mapButton.textContent === "Close Map") {
-    mapButton.textContent = "Open Map";
-    mapIframe.style.display = "none";
-  }
-});
 mapButton.addEventListener("click", function () {
   if (myFlag === false) {
     mapButton.textContent = "Close Map";
     mapIframe.style.display = "block";
+    mapHeight.style.height = "450px";
     myFlag = true;
+  } else {
+    mapButton.textContent = "Open Map";
+    mapIframe.style.display = "none";
+    mapHeight.style.height = "0";
+    myFlag = false;
   }
 });
+
+
 
 // send message button
 let sendMessageBtn = document.getElementsByClassName("message-btn")[0];
@@ -197,22 +210,39 @@ clearButton.addEventListener("click", function () {
 });
 // bg color dark
 
-// ! round image changer
+// ! ****************************************************************** round image changer
 let roundImgArr;
+const squareContentArr = [
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum minus, cum soluta asperiores quae necessitatibus.",
+  "Vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolor.",
+  "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem.",
+  "Duis aute irure dolor in relore eu fugiat nulla pariatur. Excepteur.",
+  "Dlum dolore eu fugiat nulla pariatur. Excepteur"
+];
+const nameArr = [
+  "Matthew Dix",
+  "Joshua Earle",
+  "Mike Petrucci",
+  "John Doe",
+  "Lauren Dix"
+]
 let mainImg = document.querySelector(".round-pic-absolute img");
 let squareFrameContent = document.querySelector(".round-area-text p");
 let allRoundPics = document.querySelectorAll(".round-imgs img");
 let picBorder = document.querySelectorAll(".round-imgs");
+let namesForContent = document.querySelector(".round-area-text span");
 // picBorder[0].classList.remove("round-imgs");
 picBorder[1].classList.add = "round-class-yellow";
 for (let i = 0; i < allRoundPics.length; i++) {
   // console.log(allRoundPics[i].getAttribute("src"));
   allRoundPics[i].addEventListener("click", function () {
     mainImg.setAttribute("src", allRoundPics[i].getAttribute("src"));
+    squareFrameContent.textContent = squareContentArr[i];
+    namesForContent.textContent = nameArr[i];
   });
 }
 
-// !change all buttons color
+// ! *********************************************************change all buttons color
 let orangeBtn = document.querySelectorAll(".color-btn div button")[0];
 let purpleBtn = document.querySelectorAll(".color-btn div button")[1];
 let greenBtn = document.querySelectorAll(".color-btn div button")[2];
